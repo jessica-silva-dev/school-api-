6dab3b5061730e01afd75a7cdf124feee54e405c
Jéssica Silva <jessica.akyla@gmail.com>
2025-05-09 21:03:54 -0300

    class AssessmentApiView(APIView): feat: Create viewa

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import AssessmentSerializer, CourseSerializer
+from .models import Assessment, Course
+
+class AssessmentApiView(APIView):
+    
+    def get(self, request):
+        assessment = Assessment.objects.all()
+        serializer = AssessmentSerializer(assessment, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
